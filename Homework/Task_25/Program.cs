d90d6ebf32b9bece1652c91328c9a5ec38502587
alexandergm <sasha_261@outlook.com>
2022-08-29 23:20:17 +0300

    Complete Task_25

--- Homework/Task_25/Program.cs
+++ Homework/Task_25/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 1ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Function());
+
+string Function()
+{
+    var x = Math.Pow(a,b);
+    return $"Число {a} в степени {b} = {x}";
+}
