2cc301458e687b5843556a902120d32997c2e0f1
alexandergm <sasha_261@outlook.com>
2022-08-29 23:40:57 +0300

    Complete Task_29

---
+++ Homework/Task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+FillArray();
+Console.WriteLine(PrintArray());
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+string PrintArray()
+{
+    string result = String.Empty;
+    foreach(int x in array)
+    {
+       result = result + x + " ";
+    }
+    return result;
+}
+
